c7736542cf498dfa17f62a8e5452d6c6c681d516
BloodyHair267 <micskobence3@gmail.com>
2026-08-06 10:36:11 +0200

    Update
    Deep Optimization:
    - Reduced time complexity from O(n²) to O(n√n)
    - Skips even numbers and even divisors
    - Checks divisors only up to √i for each candidate number
    - Changed output from primes.txt to .log files

---
+++ Primes/prime generator/prime generator.py
@@ -0,0 +1,68 @@
+import time
+from datetime import datetime
+
+while True:
+    try:
+        low_lim = int(input("Lower limit:\n>>> "))
+        if low_lim < 1:
+            print("Lower limit can't be smaller than 1")
+            continue
+        else:
+            break
+    except ValueError:
+        print("\nValueError. Try again\n")
+
+
+while True:
+    try:
+        up_lim = int(input("Upper limit:\n>>> "))
+
+        if up_lim < low_lim:
+            print(f"Must be higher then {low_lim}")
+        else:
+            break
+    except ValueError:
+        print("\nValueError. Try again\n")
+
+primelist = []
+
+start = time.perf_counter()
+
+for i in range(low_lim, up_lim+1):
+    if i == 2:
+        primelist.append(i)
+        continue
+
+    if i % 2 == 0:
+        continue
+
+    for j in range(3, int(i**0.5)+1, 2):
+        if i % j == 0:
+            break
+
+    else:
+        primelist.append(i)
+
+if 1 in primelist:
+    primelist.remove(1) 
+
+end = time.perf_counter()
+
+print("Finished computing")
+print(f"\nTime elapsed: {end-start:.6f} seconds")
+
+log = datetime.now().strftime(r"Primes\prime generator\logs\%Y-%m-%d_%H-%M-%S.log")
+
+with open(log, "w") as file:
+    file.write(
+
+f"""Primes from {low_lim}-{up_lim}
+
+{primelist}
+
+Total of {len(primelist)} primes
+Elapsed Time: {end-start:.6f} seconds
+Density: {100*((len(primelist))/(up_lim-low_lim+1)):.3f}%"""
+)
+
+print(f"Details in {log}")
